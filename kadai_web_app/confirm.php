9c2b91131edca7b0a72b6cbbd80e9c975f087050
ayachii0431 <ayaberu0610@gmail.com>
2026-05-04 22:52:08 +0900

    Add files via upload

--- kadai_web_app/confirm.php
+++ kadai_web_app/confirm.php
@@ -1,7 +1,7 @@
 <?php
 // POSTリクエストから入力データを取得
-$name=$_POST['user_name'];
-$age=$_POST['user_age'];
+$name=$_POST['employee_name'];
+$age=$_POST['employee_age'];
 $department=$_POST['department'];
 ?>
 
